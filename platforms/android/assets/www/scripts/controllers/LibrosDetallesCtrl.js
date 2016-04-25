d0dc593e5e7398f015f765c9393d39a609ebe5d8
Yagouus <yagofontenla95@gmail.com>
2016-04-25 14:23:34 +0200

    Statusbar color tint 700

--- platforms/android/assets/www/scripts/controllers/LibrosDetallesCtrl.js
+++ platforms/android/assets/www/scripts/controllers/LibrosDetallesCtrl.js
@@ -1,5 +1,6 @@
 angular.module("Drungap").controller("LibrosDetallesCtrl", ["$scope", "$location", "Libro", "ApiService", function ($scope, $location, Libro, ApiService){
 
+    //Send to scope
     $scope.libro = Libro.data[0];
 
 }]);
--- platforms/android/assets/www/scripts/services/ApiService.js
+++ platforms/android/assets/www/scripts/services/ApiService.js
@@ -1,6 +1,7 @@
-//Servicio que se conecta a la Api
+//Service that connects to API
 angular.module("Drungap").service("ApiService", ["$http" , "settings",function ($http, settings) {
 
+    //Query the API
     this.consultaApi = function (servicio) {
         return $http.get(settings.rutaApi + settings.version + servicio);
     };
